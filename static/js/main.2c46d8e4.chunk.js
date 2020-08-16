f1c316a0fdc597833863629129a37bac86251715
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-16 13:18:19 -0300

    teste deploy branch

---
+++ static/js/main.2c46d8e4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2);var l=function(){return r.a.createElement("header",null,"Div Header")};var u=function(){return r.a.createElement("div",{className:"App"},r.a.createElement(l,null))};n.n(c).a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(u,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.2c46d8e4.chunk.js.map
