bfdce147901d77a897a07c2efb862ea49e6f60ca
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-16 13:43:06 -0300

    header estilizado

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import './styles.css';
+
 function Header() {
     return (
-        <header>Div Header</header>
+        <header>
+            <div className="box">
+                <p>Eduardo Correia</p>
+                <div className="links">
+                    <a href="">blog</a>
+                    <a href="">contato</a>
+                    <a href="">projetos</a>
+                </div>
+            </div>
+        </header>
     )
 }
 
