d426c422bc96b40203eb5c2b0012ab3c7641630a
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-18 21:17:23 -0300

    header com dropdown estilizado

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
+import menuIcon from '../../assets/menu.svg';
+import projectsIcon from '../../assets/projects.svg';
+import contactIcon from '../../assets/contact.svg';
+import blogIcon from '../../assets/blog.svg';
+
 function Header() {
+    const [open, setOpen] = useState(false);
+
+    const openDropdown = () => {
+        setOpen(!open);
+    }
+
     return (
         <header>
             <div className="box">
-                <p>Eduardo Correia</p>
-                <div className="links">
-                    <a href="">projetos</a>
-                    <a href="">blog</a>
-                    <a href="">contato</a>
-                </div>
+                <p className="name">Eduardo Correia</p>
+
+                <a href="#" className="dropdown" onClick={openDropdown}>
+                    <img src={menuIcon} alt=""/>
+                </a>
+
+                {open && <div className="menu">
+                    <a href="#" className="link">
+                        <img src={projectsIcon} alt=""/>
+                        projetos
+                    </a>
+                    <a href="#" className="link">
+                        <img src={blogIcon} alt=""/>
+                        blog
+                    </a>
+                    <a href="#" className="link">
+                        <img src={contactIcon} alt=""/>
+                        contato
+                    </a>
+                </div>}
             </div>
         </header>
     )
