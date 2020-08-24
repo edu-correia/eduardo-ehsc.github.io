eff1e02e892eca9171ca532fe21c790b2e8e1dd0
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-23 23:06:40 -0300

    Base da página do blog

---
+++ web/src/components/BlogContent/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BlogContent(){
+    return (
+        <h1>Blog</h1>
+    )
+}
+
+export default BlogContent;
---
+++ web/src/pages/BlogPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import BlogContent from '../../components/BlogContent';
+
+function BlogPage(){
+    return (
+        <div>
+            <Header />
+            <BlogContent />
+        </div>
+    )
+}
+
+export default BlogPage;
