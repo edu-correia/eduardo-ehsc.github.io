3ccad2dc9b7d823bb6df63244da7f3a5f38ccaea
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-25 11:51:13 -0300

    BlogItem e togglePost
    Componente BlogItem e lógica de abertura do post

---
+++ web/src/components/BlogContent/components/BlogItem/index.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+import './styles.css';
+
+import quoteIcon from '../../../../assets/quote.svg';
+import arrowDownIcon from '../../../../assets/arrow-down.svg';
+import arrowUpIcon from '../../../../assets/arrow-up.svg';
+
+function BlogItem(){
+    const [toggle, setToggle] = useState(false);
+
+    return(
+        <div className="blog-item">
+            <div className="post-title">
+                Uso de styled-components
+                <button onClick={() => (setToggle(!toggle))}>
+                    <img src={toggle === false ? arrowDownIcon : arrowUpIcon} alt="" className="toggle"/>
+                </button>
+            </div>
+            <div className="post-languages">
+                <div className="post-language">
+                    JavaScript
+                </div>
+
+                <div className="post-language">
+                    TypeScript
+                </div>
+            </div>
+            {toggle && <span className="post-content">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ad corrupti asperiores vel sit incidunt eveniet beatae. Quasi placeat magnam iste ullam perferendis dolore ab, consectetur aliquid, quod id quas quibusdam provident vel magni eius labore debitis. Aspernatur, quaerat odio?
+            </span>}
+            <img src={quoteIcon} alt="" className="quotes"/>
+        </div>  
+    )
+}
+
+export default BlogItem;
--- web/src/components/BlogContent/index.tsx
+++ web/src/components/BlogContent/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import BlogItem from './components/BlogItem';
+
 import './styles.css';
 
 import plusIcon from '../../assets/plus.svg';
-import quoteIcon from '../../assets/quote.svg';
-import arrowDownIcon from '../../assets/arrow-down.svg';
 
 function BlogContent(){
     return (
@@ -17,49 +17,10 @@ function BlogContent(){
                 </button>
             </div>
 
-            <div className="blog-item">
-                <div className="post-title">
-                    Uso de styled-components
-                    <button>
-                        <img src={arrowDownIcon} alt="" className="toggle"/>
-                    </button>
-                </div>
-                <div className="post-languages">
-                    <div className="post-language">
-                        JavaScript
-                    </div>
-
-                    <div className="post-language">
-                        TypeScript
-                    </div>
-                </div>
-                <span className="post-content">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ad corrupti asperiores vel sit incidunt eveniet beatae. Quasi placeat magnam iste ullam perferendis dolore ab, consectetur aliquid, quod id quas quibusdam provident vel magni eius labore debitis. Aspernatur, quaerat odio?
-                </span>
-                <img src={quoteIcon} alt="" className="quotes"/>
-            </div>
-
-            <div className="blog-item">
-                <div className="post-title">
-                    Uso de styled-components
-                    <button>
-                        <img src={arrowDownIcon} alt="" className="toggle"/>
-                    </button>
-                </div>
-                <div className="post-languages">
-                    <div className="post-language">
-                        JavaScript
-                    </div>
-
-                    <div className="post-language">
-                        TypeScript
-                    </div>
-                </div>
-                <span className="post-content">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ad corrupti asperiores vel sit incidunt eveniet beatae. Quasi placeat magnam iste ullam perferendis dolore ab, consectetur aliquid, quod id quas quibusdam provident vel magni eius labore debitis. Aspernatur, quaerat odio?
-                </span>
-                <img src={quoteIcon} alt="" className="quotes"/>
-            </div>
+            <BlogItem />
+            <BlogItem />
+            <BlogItem />
+            <BlogItem />
         </div>
     )
 }
