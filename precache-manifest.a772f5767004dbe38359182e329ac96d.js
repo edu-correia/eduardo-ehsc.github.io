6633029f8101f00e48a82bd73b037bb634ecf1d7
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-09-02 21:40:20 -0300

    Updates

---
+++ precache-manifest.a772f5767004dbe38359182e329ac96d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af87181d1dec0e57620fce1c6c2adf2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca45a46e391ccb31e40f",
+    "url": "/static/css/main.8a698186.chunk.css"
+  },
+  {
+    "revision": "bf46bc246b666a18366b",
+    "url": "/static/js/2.d17a5595.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d17a5595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca45a46e391ccb31e40f",
+    "url": "/static/js/main.c4fe7c76.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "e754c83ce99a9912f0d61ec125ede04f",
+    "url": "/static/media/arrow-down.e754c83c.svg"
+  },
+  {
+    "revision": "7381b8d14a5ec665307c2968261c277e",
+    "url": "/static/media/arrow-up.7381b8d1.svg"
+  },
+  {
+    "revision": "179b71cb8ed2ec36d47c563e8313424c",
+    "url": "/static/media/blog.179b71cb.svg"
+  },
+  {
+    "revision": "37b7a85e7a8fad9e3f8c1cf6463e7f06",
+    "url": "/static/media/contact.37b7a85e.svg"
+  },
+  {
+    "revision": "38445e8d7100f8d3d62c71c7013a6a7c",
+    "url": "/static/media/css.38445e8d.svg"
+  },
+  {
+    "revision": "99325d4efd59006419033a0523a5b0ba",
+    "url": "/static/media/html.99325d4e.svg"
+  },
+  {
+    "revision": "3094c4233431edb41ad689a35effadc8",
+    "url": "/static/media/javascript.3094c423.svg"
+  },
+  {
+    "revision": "cdc525fef1db60b5ba5c2524d80a06ce",
+    "url": "/static/media/node.cdc525fe.svg"
+  },
+  {
+    "revision": "558b22c60f37141211b90032bd8dce1e",
+    "url": "/static/media/plus.558b22c6.svg"
+  },
+  {
+    "revision": "67b0ad8bf63265f04718c26d6ae6fe43",
+    "url": "/static/media/projects.67b0ad8b.svg"
+  },
+  {
+    "revision": "49d3d0989c70b777ea762bfd82783304",
+    "url": "/static/media/quote.49d3d098.svg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/static/media/react.d8cf7740.svg"
+  },
+  {
+    "revision": "b3fc4d18d04298bf21b6ca872e718da0",
+    "url": "/static/media/star.b3fc4d18.svg"
+  }
+]);
